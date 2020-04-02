987285d06c4ae653bcdb4b4ee780c2fcbd91fa16
gordon tang <gordon@gordons-MBP.broadband>
2020-04-02 10:38:52 +0100

    created new project

--- src/App.js
+++ src/App.js
@@ -2,23 +2,48 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import './homepage.styles.scss';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HATS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">JACKETS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">SNEAKERS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">WOMENS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">MENS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
