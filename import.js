c466af1d56a3cffef92f4677cef42c5897e1ab4e
gordon tang <gordon@gordons-MBP.broadband>
2020-04-06 18:23:12 +0100

    section 14

---
+++ import.js
@@ -0,0 +1,10 @@
+import { version } from "react"
+https://crown-clothing-gt.herokuapp.com/
+
+sesion 14, heroku 
+{
+    heroku --versionh
+    heroku login 
+    heroku create crown-clothing-gt --buildpack https://github.com/mars/create-react-app-buildpack
+    heroku push heroku master
+}
--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -4,7 +4,12 @@ import Logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middleWares = [Logger];
+const middleWares = [];
+
+if (process.env.NODE_ENV === 'development') {
+    middleWares.push(Logger);
+}
+
 
 export const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
